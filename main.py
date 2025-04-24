4828cea5310d7acb3b04da8e68c392d95db3e05f
luisamichelinr <luisa.michelinr@gmail.com>
2025-04-24 15:29:23 -0300

    Upload do projeto de calculadora de IMC

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calcular_imc', methods=['POST'])
+def calcular_imc():
+    altura = float(request.form['altura'])
+    peso = float(request.form['peso'])
+
+    imc = round(peso / (altura ** 2), 2)
+
+    if imc < 18.5:
+        diagnostico = 'Abaixo do peso'
+    elif imc < 24.9:
+        diagnostico = 'Peso normal'
+    elif imc < 29.9:
+        diagnostico = 'Acima do peso'
+    else:
+        diagnostico = 'Obesidade'
+
+    return render_template('index.html', imc = imc, diagnostico = diagnostico)
+
+if __name__ == '__main__':
+    app.run(debug=True)
